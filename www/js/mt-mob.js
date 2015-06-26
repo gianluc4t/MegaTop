d4fda37269af29210d3230e74a1afceb62ecc9a5
GT <gianluc4t@gmail.com>
2015-06-26 19:21:14 -0300

    entry commit

---
+++ www/js/mt-mob.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+
+    $("#crs-categorias").owlCarousel({
+        itemsMobile : [640,2],
+        autoPlay : true,
+        stopOnHover : true
+    });
+
+    $("#crs-melhores-ofertas").owlCarousel({
+        itemsMobile : [640,2],
+        autoPlay : true,
+        stopOnHover : true
+    });
+
+    $("#crs-lojas").owlCarousel({
+        itemsMobile : [640,3],
+        autoPlay : true,
+        stopOnHover : true
+    });
+
+    $("#crs-marcas").owlCarousel({
+        itemsMobile : [640,3],
+        autoPlay : true,
+        stopOnHover : true
+    });
+
+});
